d0a06d42b5d22e36367848a9336bd824f406cec7
CathalRyan96 <cathalryan48@gmail.com>
2017-09-28 11:47:23 +0100

    Initial commit

---
+++ CurrentTime.go
@@ -0,0 +1,14 @@
+//Author Cathal Ryan
+//Adopted from https://tour.golang.org/welcome/4
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("This program displays the current time!")
+
+	fmt.Println("The time is", time.Now())
+}
