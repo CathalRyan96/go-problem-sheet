1ab3ba4803b700bb328c45e7be1e2d6cf0b60aa4
CathalRyan96 <cathalryan48@gmail.com>
2017-10-06 11:32:06 +0100

    added a function that initializes two varibles and calculates if he string entered is a palidrome

--- PalimdromeTest.go
+++ PalimdromeTest.go
@@ -3,13 +3,27 @@ package main
 import "fmt"
 import "strings"
 
+//main function that takes in users string
 func main () {
 
-	var test strings
+	var ip string
 	fmt.Println("Enter String to see if it is a Palimdrome!:")
-	fmt.Scanf("%s\n",test)
-	test = strings.ToLower(test)
-	fmt.Println(isP(test))
+	fmt.Scanf("%s\n", &ip)
+	ip = strings.ToLower(ip)
+	fmt.Println(isP(ip))
 
 	
+}
+
+func isP(s string) string{
+	mid := len(s) / 2
+	last := len(s) - 1
+
+	for i := 0; i< mid; i++ {
+		if s[i] != s[last-i]{
+		return "No, this is not a palidrome"			
+		}
+		
+	}
+		return "Yes, this is a palidrome"
 }
